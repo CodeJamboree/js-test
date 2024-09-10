9557972e0b9f6f7920371aaad1d84fc0e4d8d3fa
Lewis Moten <lewis.moten@codejamboree.com>
2024-09-10 19:46:04 -0400

    handle cleanup errors

--- src/runTest.ts
+++ src/runTest.ts
@@ -1,14 +1,30 @@
 import { ExpectationError } from "./expect/ExpectationError.js";
-import { RunningState, SuiteSetup, TestFunction, TestState } from "./global.js";
+import { RunningState, SetupFunction, SuiteSetup, TestFunction, TestState } from "./global.js";
 import { logFailing } from "./log/logFailing.js";
 import { logPassing } from "./log/logPassing.js";
 
 const maxSignedInt32 = 0x7FFFFFFF;
+
+interface Cleanup {
+  test: boolean,
+  suite: boolean,
+  run: boolean,
+  errors: unknown[]
+}
 export const runTest = async (test: TestFunction, setup: SuiteSetup, testState: TestState, runningState: RunningState) => {
+  const clean = {
+    test: false,
+    suite: false,
+    run: false,
+    errors: []
+  }
   try {
     await runningState.beforeEach?.();
+    clean.run = true;
     await setup.beforeEach?.();
+    clean.suite = true;
     await test.before?.();
+    clean.test = true;
     let { timeoutMs } = runningState;
     if ('timeoutMs' in test) {
       timeoutMs = test.timeoutMs as number;
@@ -37,20 +53,41 @@ export const runTest = async (test: TestFunction, setup: SuiteSetup, testState: 
         clearTimeout(timeout);
       }
     });
-    await test.after?.();
-    await setup.afterEach?.();
-    await runningState.afterEach?.();
+    if (clean.test) {
+      clean.test = false;
+      await invokeWithoutThrowing(test.after, 'test.after', clean.errors);
+    }
+    if (clean.suite) {
+      clean.suite = false;
+      await invokeWithoutThrowing(setup.afterEach, 'testSuite.afterEach', clean.errors);
+    }
+    if (clean.run) {
+      clean.run = false;
+      await invokeWithoutThrowing(runningState.afterEach, 'runningState.afterEach', clean.errors);
+    }
     if (![maxSignedInt32, 0].includes(timeoutMs) && durationMs > timeoutMs) {
       throw new ExpectationError(durationMs, 'greater than', timeoutMs, 'timeout');
     }
+    if (clean.errors.length !== 0)
+      throw new ExpectationError('', 'threw', clean.errors, 'cleanup');
     runningState.passed++;
     logPassing(testState, runningState);
   } catch (error) {
-    await setup.afterEach?.();
-    await runningState.afterEach?.();
+    if (clean.test) await invokeWithoutThrowing(test.after, 'test.after', clean.errors);
+    if (clean.suite) await invokeWithoutThrowing(setup.afterEach, 'testSuite.afterEach', clean.errors);
+    if (clean.run) await invokeWithoutThrowing(runningState.afterEach, 'runningState.afterEach', clean.errors);
     testState.error = error;
     runningState.failures.push(testState);
     runningState.failed++;
     logFailing(testState, runningState);
+  }
+}
+const invokeWithoutThrowing = async (callback: SetupFunction | undefined, name: string, errors: unknown[]): Promise<void> => {
+  if (!callback) return;
+  try {
+    await callback();
+  } catch (e) {
+    errors.push(e);
+    console.error(`Failed to invoke ${name}`);
   }
 }
